09b35dcb15c3f9ee5dd5c8c0b570987122b6d061
frankiehuangg <frankie.huangg@gmail.com>
2022-04-23 23:03:47 +0700

    rearranged lines

--- F14.py
+++ F14.py
@@ -1,6 +1,5 @@
 def helpadmin():
     print ('==========================Menu Bantuan Bagi Admin==========================')
-    print ('>>>')
     print ('1. register             - Untuk melakukan registrasi user baru')
     print ('2. login                - Untuk melakukan login ke dalam sistem')
     print ('3. tambah_game          - Untuk menambah game yang dijual pada toko')
@@ -9,16 +8,15 @@ def helpadmin():
     print ('6. list_game_toko       - Untuk melihat list game yang dijual pada toko')
     print ('7. search_game_at_store - Untuk Mencari Game yang ada di Toko')
     print ('8. topup                - Untuk menambahkan saldo kepada user ')
-    print ('9. help                 - Untuk memberikan panduan penggunaan sistem')
-    print ('10. save                - untuk menyimpan data setelah dilakukan perubahan')
-    print ('11. exit                - Untuk menghentikan program')
-    print ('12. kerangajaib         - Untuk bertanya kepada kerang ajaib')
-    print ('13. tictactoe           - Untuk bermain tic tac toe')
+    print ('9. kerangajaib          - Untuk bertanya kepada kerang ajaib')
+    print ('10. tictactoe           - Untuk bermain tic tac toe')
+    print ('11. help                - Untuk memberikan panduan penggunaan sistem')
+    print ('12. save                - untuk menyimpan data setelah dilakukan perubahan')
+    print ('13. exit                - Untuk menghentikan program')
     return
 
 def helpuser() :
     print ('==========================Menu Bantuan Bagi User==========================')
-    print ('>>>')
     print ('1. login                - Untuk melakukan login ke dalam sistem')
     print ('2. list_game_toko       - Untuk melihat list game yang dijual pada toko')
     print ('3. buy_game             - Untuk membeli game yang diinginkan')
@@ -26,15 +24,15 @@ def helpuser() :
     print ('4. search_my_game       - Untuk mencari game yang dimiliki oleh pengguna')
     print ('5. search_game_at_store - Untuk Mencari Game yang ada di Toko')
     print ('6. riwayat              - Untuk melihat riwayat pembelian game pengguna')
-    print ('7. help                 - Untuk memberikan panduan penggunaan sistem')
-    print ('8. save                 - untuk menyimpan data setelah dilakukan perubahan ')
-    print ('9. exit                - Untuk menghentikan program')
-    print ('10. kerangajaib         - Untuk bertanya kepada kerang ajaib')
-    print ('11. tictactoe           - Untuk bermain tic tac toe')
+    print ('7. kerangajaib          - Untuk bertanya kepada kerang ajaib')
+    print ('8. tictactoe            - Untuk bermain tic tac toe')
+    print ('9. help                 - Untuk memberikan panduan penggunaan sistem')
+    print ('10. save                - untuk menyimpan data setelah dilakukan perubahan ')
+    print ('11. exit                - Untuk menghentikan program')
     return
     
-def help(user_id) :
-    if (user_id == 'user') :
+def help(role) :
+    if (role == 'user') :
         helpuser()
     else:
         helpadmin()
