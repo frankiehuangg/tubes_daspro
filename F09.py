12236c0f794b699fe8c3adcdfcf3860123ffcc4c
mrsyaban <95738230+mrsyaban@users.noreply.github.com>
2022-04-25 00:54:54 +0800

    Update F09.py

--- F09.py
+++ F09.py
@@ -1,28 +1,50 @@
+# Program melihat game yang dimiliki
+# menampilkan daftar game yang dimiliki pengguna
+
 import functions.basic as basic,  functions.files as files
 
+# KAMUS
+# jumlah_game : int
+# 
+
+# fungsi untuk mengecek apakah user memiliki game
 def cek_daftarGame(user_id, kepemilikan_array):
+    # KAMUS LOKAL
+    # tidak punya : bool
+    # kepemilikan_array : array
+    # user_ID : str
+
+    # ALGORITMA
     tidak_punya = True
-    kepemilikan = kepemilikan_array
-    for row in kepemilikan :
+    for row in kepemilikan_array :
         user_ID = row[1]
         if user_ID == user_id :
             tidak_punya = False
             break
     return tidak_punya
             
+# fungsi untuk mengembalikan array daftar game yang dimiliki user
 def list_user_game(user_id, riwayat_array):
+    # KAMUS LOKAL
+    # jumlah_game, count : int
+    # riwayat_array, Game, daftar_game_user : array
+    # user_ID, riwayat_game_id, game_tahun_beli : str
+    # game_id, game_nama, game_kategori, game_tahun_rilis, game_harga : str
+
+    # ALGORITMA
     global jumlah_game
     jumlah_game = 0
     count = -1
     # urutan : no, ID game, nama game, kategori, tahun rilis, tahun beli, harga
-    Riwayat = riwayat_array
     Game = files.csv_to_2d_array("files/game.csv")
-    for row in Riwayat :
+    for row in riwayat_array :
         user_ID = row[3]
         if user_ID == user_id :
             jumlah_game += 1
     daftar_game_user = [["" for i in range(7)] for i in range(jumlah_game)]
-    for row in Riwayat :
+
+    # memasukkan elemen-elemen game yang dimiliki user ke array
+    for row in riwayat_array :
         user_ID = row[3]
         riwayat_game_id = row[0]
         game_tahun_beli = row[4]
@@ -45,13 +67,24 @@ def list_user_game(user_id, riwayat_array):
                     break
     return daftar_game_user
 
+# fungsi untuk menampilkan array daftar game yang dimiliki user
 def list_game(user_id, kepemilikan_array, riwayat_array):
+    # KAMUS LOKAL
+    # daftar_game_user : array
+    # elemen : str
+    # jumlah_game : int
+
+    # ALGORITMA
+    # tampilkan judul tabel
     print("-"*113)
     print(" "*52 +"GAME KAMU"+ " "*52)
+
     # cek apakah user sudah memiliki game
     if cek_daftarGame(user_id, kepemilikan_array):
         print("-"*113)
-        print("\nMaaf, kamu tidak memiliki game! yuk beli game dulu dengan ketik perintah 'buy_game'")
+        print(" "*15 +"\nMaaf, kamu tidak memiliki game! yuk beli game dulu dengan ketik perintah 'buy_game'" + " "*15)
+
+    # menampilkan game yang dimiliki user
     else :
         daftar_game_user = list_user_game(user_id, riwayat_array)
         print("-"*113)
